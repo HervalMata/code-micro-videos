b1ba9dce9da69df81532608cc3c8c90e9fd1643b
HervalMata <hervalmataamparo@gmail.com>
2020-03-31 14:15:59 -0300

    Resource Categories Get

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,6 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::group(['namespace' => 'api'], function () {
+Route::group(['namespace' => 'Api'], function () {
     Route::resource('categories', 'CategoryController', ['except' => ['create', 'edit']]);
 });
