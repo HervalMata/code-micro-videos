a891d09bde6ecd8d519bf22ab9f73368c1360084
HervalMata <hervalmataamparo@gmail.com>
2020-04-06 22:15:20 -0300

    Resource Category Delete

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\CategoryRequest;
 use App\Models\Category;
+use Exception;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Response;
 
@@ -60,9 +61,11 @@ class CategoryController extends Controller
      *
      * @param Category $category
      * @return Response
+     * @throws Exception
      */
     public function destroy(Category $category)
     {
-        //
+        $category->delete();
+        return \response()->noContent();
     }
 }
