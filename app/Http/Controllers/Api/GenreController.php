854f02e0caeb757b0dcd98ce102828bcd606c7ee
HervalMata <hervalmataamparo@gmail.com>
2020-04-07 22:51:47 -0300

    Movendo GenreController para Api

--- app/Http/Controllers/Api/GenreController.php
+++ app/Http/Controllers/Api/GenreController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use App\Models\Genre;
 use Exception;
 use Illuminate\Database\Eloquent\Collection;
